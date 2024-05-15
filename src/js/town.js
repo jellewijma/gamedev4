241719bc5e3d641f6b7802b9e22c4f14ce73ac82
Jelle <jelle.wijma@gmail.com>
2024-05-15 09:11:37 +0200

    move func (copilot)

--- src/js/town.js
+++ src/js/town.js
@@ -1,12 +1,12 @@
 import '../css/style.css'
 import { Actor, Engine, Vector, Loader, Font, Text, Rectangle, Color, GraphicsGroup, Direction, BaseAlign, TextAlign, vec, Scene, Random, Label } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
-
+import { Hero } from './hero.js'
 
 class Town extends Scene {
     constructor() {
         super()
-        console.log('Tower scene created')
+        console.log('Town scene created')
 
         const summeningHeroeButton = new Label({
             text: "Summon Hero",
@@ -25,10 +25,32 @@ class Town extends Scene {
         summeningHeroeButton.on('pointerdown', () => {
             this.summonHero()
         })
+        this.add(summeningHeroeButton)
+
+        this.setRandomDestination();
     }
+
+    onPostUpdate(engine, delta) {
+        if (this.pos.distance(this.destination) < 5) { // If the hero is close to the destination
+            this.setRandomDestination(engine);
+        } else {
+            const direction = this.destination.sub(this.pos).normalize(); // Get the direction to the destination
+            this.vel = direction.scale(100); // Move the hero in the direction of the destination
+        }
+    }
+
+    setRandomDestination(engine) {
+        const randomX = Random.between(0, engine.drawWidth);
+        const randomY = Random.between(0, engine.drawHeight);
+        this.destination = new Vector(randomX, randomY);
+    }
+
 
     summonHero() {
         console.log('drawing hero')
+        const hero = new Hero()
+        this.add(hero)
+        console.log(hero)
     }
 }
 
